02822adf7e18d1f690f9471aa0e0da109790d901
GianlucaBortoli <giallogiallo93@gmail.com>
2016-12-23 10:58:19 +0100

    update requiments & added crawler & some wikipedia test urls

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # topic-zoomer
-Big Data course project
+This is the project for the Big Data course at the University of Trento.
+
+The goal is to categorise geolocalised URLs. It is possible to select an area of
+interest by means of a top left and a bottom right point and a step S to further
+divide this area into squares (of size SxS).
 
 # Requirements
 * python3
@@ -7,8 +11,14 @@ Big Data course project
 * working spark environment
 
 # How to run the tool
-## Example
+## Example to generate the dataset from URL file
 ```bash
-cd /path/to/repo/src`
-`/path/to/spark-submit topic_zoomer.py --tlx 0 --tly 100 --brx 100 --bry 0 --step 50 --dataset ../data/test.csv
+cd /path/to/repo/data
+python crawler.py --input 100_wikipedia_urls --output out.csv --min 0 --max 100
+```
+
+## Example to run the topic extractor on Spark
+```bash
+cd /path/to/repo/src
+/path/to/spark-submit topic_zoomer.py --tlx 0 --tly 100 --brx 100 --bry 0 --step 50 --dataset ../data/test.csv
 ```
---
+++ data/crawler.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env
+from multiprocessing.pool import ThreadPool
+from queue import Queue
+from bs4 import BeautifulSoup
+import argparse, requests, os, sys
+import csv, random
+
+# global vars
+concurrent = 100
+q = Queue(concurrent * 2)
+maxFieldSize = 131072
+
+def downloadPage(url):
+    strippedUrl = url.strip()
+    try:
+        r = requests.get(strippedUrl)
+        if (r.status_code == 200):
+            print("\t", strippedUrl)
+            body = r.text
+            soup = BeautifulSoup(body, 'html.parser')
+            # remove script & style parts
+            for s in soup(["script", "style"]):
+                s.extract()
+            # extract text from body
+            text = soup.get_text()
+            # break into lines and remove leading and trailing space on each
+            lines = (line.strip() for line in text.splitlines())
+            # break multi-headlines into a line each
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            # drop blank lines
+            strippedText = ''.join(chunk for chunk in chunks if chunk)
+            # truncate string at max field size allowed in csv python module
+            return strippedText[:maxFieldSize]
+        else:
+            print("Skipping (status code = {})".format(r.status_code))
+    except Exception as e:
+        print(e)
+
+def main(inputFile, outputFile, _min, _max):
+    urls = []
+    with open(inputFile) as i:
+        urls = i.readlines()
+    print('Downloading {} pages...'.format(len(urls)))
+    print('Min = ', _min)
+    print('Max = ', _max)
+
+    try:
+        results = ThreadPool(concurrent).imap_unordered(downloadPage, urls)
+
+        with open(outputFile, 'w+') as o:
+            fieldnames = ['x', 'y', 'text']
+            writer = csv.DictWriter(o, delimiter=',', fieldnames=fieldnames)
+            writer.writeheader()
+            for t in results:
+                # write text back into csv with fake geotags
+                writer.writerow({
+                    'x': int(random.uniform(_min, _max)),
+                    'y': int(random.uniform(_min, _max)),
+                    'text': t
+                })
+    except KeyboardInterrupt:
+        print("\nStopped by user")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    # command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, help='the input file with the URLs', required=True)
+    parser.add_argument('--output', type=str, help='the output csv file', required=True)
+    parser.add_argument('--min', type=int, help='the min value for generating fake geotags', required=True)
+    parser.add_argument('--max', type=int, help='the max value for generating fake geotags', required=True)
+    args = parser.parse_args()
+    # get abs path from file
+    ifilePath = os.path.abspath(args.input)
+    ofilePath = os.path.abspath(args.output) 
+    main(ifilePath, ofilePath, args.min, args.max)
